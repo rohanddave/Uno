412cd34f4a3c0cdd73724a97a55ee2d0f94cce7a
rohandewangdave <rohandewangdave@gmail.com>
2020-07-07 04:08:02 +0530

    basic setup

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import random
 class Card:
     def __init__(self, color, number):
         self.color = color
@@ -9,17 +10,23 @@ class Card:
         return False
 
 class Player:
-    def __init__(self,socket,addr):
+    def __init__(self,socket):
         self.socket = socket
-        self.addr = addr
+        #self.addr = addr
         self.cards = []
+
+    def show_cards(self):
+        for card in self.cards:
+            print(f"Color:{card.color} Number:{card.number}")
 
 
 class Game:
-    def __init__(self):
+    def __init__(self,players):
         self.normal_cards = []
         self.special_cards = []
         self.available_cards = []
+        self.selected_cards = []
+        self.all_cards = []
         self.players = []
 
         normal_indexes = (0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12,12)  # 10 = skip , 11 = draw two , 12 = reverse
@@ -33,10 +40,33 @@ class Game:
         for s_index in special_indexes:
             self.special_cards.append(Card('special', s_index))
 
-        self.available_cards = self.normal_cards + self.special_cards
-        #print(len(self.available_cards))
-        print("Game initialised")
+        self.all_cards = self.normal_cards + self.special_cards
+        self.available_cards = self.all_cards
 
-    def add_player(self,player):
-        print(f"{player.addr[0]} added! Socket:{player.socket}")
-        self.players.append(player)
+        #print(len(self.available_cards))
+        print("Game initialised!")
+        self.add_players(players)
+        self.show_players()
+        self.deal_cards()
+
+    def add_players(self,player_sockets):
+        for player_socket in player_sockets:
+            self.players.append(Player(player_socket))
+
+    def show_players(self):
+        for player in self.players:
+            print(f"{player} added!")
+            #self.players.append(player)
+        print(f"Number of players in game:{len(self.players)}")
+
+    def deal_cards(self):
+        for player in self.players:
+            player.cards.extend(random.choices(self.available_cards,k=7))
+            self.selected_cards+=player.cards
+            for card in self.selected_cards:
+                if card in self.available_cards:
+                    self.available_cards.remove(card)
+
+        for player in self.players:
+            print("CARDS FOR PLAYER:")
+            player.show_cards()
--- server.py
+++ server.py
@@ -2,50 +2,67 @@ import socket
 from _thread import *
 from game import Game
 from game import Player
+import tkinter as tk
 
-uno = Game()
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#uno = Game()
 
-host = "192.168.1.17"
-port = 5555
+class Server:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.list_of_clients = []
 
-try:
-    server_socket.bind((host,port))
-except socket.error as e:
-    print(str(e))
+        self.host = "192.168.1.17"
+        self.port = 5555
 
-server_socket.listen(4)
-
-list_of_clients = []
-
-def threaded_client(client_socket , addr):
-    player_client = Player(client_socket,addr)
-    uno.add_player(player_client)
-    client_socket.send(str.encode("You were added to the game!"))
-    while True:
         try:
-            message = client_socket.recv(1024)
-            if message:
-                print(f"<{addr[0]}>{message.decode()}")
-            else:
-                print("Removing:",addr[0])
-                list_of_clients.remove(client_socket)
-        except Exception as e:
+            self.server_socket.bind((self.host,self.port))
+        except socket.error as e:
             print(str(e))
-            break
 
+        self.server_socket.listen(4)
 
-def broadcast(message, client_socket):
-    for client in list_of_clients:
-        if client == client_socket:
+    def threaded_client(self,client_socket , addr):
+        #player_client = Player(client_socket,addr)
+        #uno.add_player(player_client)
+        client_socket.send(str.encode("You joined the server!"))
+        while True:
             try:
-                client.send(message)
-            except:
-                client.close()
-                list_of_clients.remove(client)
+                message = client_socket.recv(1024)
+                if message:
+                    print(f"<{addr[0]}>{message.decode()}")
+                else:
+                    print("Removing:",addr[0])
+                    self.list_of_clients.remove(client_socket)
+            except Exception as e:
+                print(str(e))
+                break
 
+
+    def broadcast(self,message, client_socket):
+        for client in self.list_of_clients:
+            if client == client_socket:
+                try:
+                    client.send(message)
+                except:
+                    client.close()
+                    self.list_of_clients.remove(client)
+
+    def start_game(self):
+        game = Game(self.list_of_clients)
+
+
+server = Server()
+
+def tkinter():
+    window = tk.Tk()
+    b = tk.Button(window , text = "Start",command = server.start_game)
+    b.pack()
+    window.mainloop()
+
+tkinter_thread = start_new_thread(tkinter,())
 while True:
-    client_socket , addr = server_socket.accept()
-    list_of_clients.append(client_socket)
+    print("Server listening for connections.....")
+    client_socket , addr = server.server_socket.accept()
+    server.list_of_clients.append(client_socket)
     print(f"{addr} connected!")
-    start_new_thread(threaded_client , (client_socket,addr))
+    start_new_thread(server.threaded_client , (client_socket,addr))
